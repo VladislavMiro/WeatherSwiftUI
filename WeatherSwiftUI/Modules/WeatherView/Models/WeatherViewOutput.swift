66559f4d89500576e0008e176e81d3d91f3af678
Vladislav Miroshnichenko <vladislavmiro1@gmail.com>
2026-04-20 18:22:29 +0300

    Output data model for WeatherView has been added.

--- WeatherSwiftUI/Modules/WeatherView/Models/WeatherViewOutput.swift
+++ WeatherSwiftUI/Modules/WeatherView/Models/WeatherViewOutput.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+struct WeatherViewOutput {
+    
+    var header: Header = .init()
+    var dayForecast: [DayForecastCell] = []
+    var weekForecast: [WeekForecast] = []
+    var airCondition: AirCondition = .init()
+    
+    struct Header {
+        var regionName: String = "--"
+        var temperature: String = "0" + Symbols.celciusSymbol.description
+        var description: String = "---"
+        var icon: String = "d119"
+    }
+    
+    struct DayForecastCell: Hashable {
+        var time: String
+        var temperature: String
+        var icon: String
+    }
+    
+    struct WeekForecast {
+        var day: String
+        var icon: String
+        var condition: String
+        var temperature: String
+    }
+    
+    struct AirCondition {
+        var wind: String = "0" + Symbols.kmPerHour.description
+        var chanceOfRain: String = "0" + Symbols.precent.description
+        var realFeel: String = "0" + Symbols.celciusSymbol.description
+        var uvIndex: String = "0"
+    }
+    
+}
--- WeatherSwiftUI/Modules/WeatherView/Models/WeatherViewState.swift
+++ WeatherSwiftUI/Modules/WeatherView/Models/WeatherViewState.swift
@@ -11,5 +11,6 @@ struct WeatherViewState {
     var isRefreshing: Bool = false
     var isError: Bool = false
     var errorMessage: String = ""
-    var coordinates: Coordinates? 
+    var coordinates: Coordinates?
+    var data: WeatherViewOutput = .init()
 }
